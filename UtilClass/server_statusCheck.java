005ae1a34a8e87f378730e9bdf290e3a37ccb57e
hunjeongPark <88864771+hunjeongPark@users.noreply.github.com>
2022-01-21 09:56:13 +0900

    Create server_statusCheck.java

---
+++ UtilClass/server_statusCheck.java
@@ -0,0 +1,25 @@
+//CallBack Server Http Status Check (200, 500...) 
+@RequestMapping(value = "/statusCheck", method = RequestMethod.GET)
+public ResponseEntity<String> statusCheck() throws Exception {
+  return new ResponseEntity<String>(HttpStatus.OK);
+}
+
+//Call Status Check Ex
+@RequestMapping(value = "/middleServerStatusCheck", method = RequestMethod.GET)
+public @ResponseBody
+String middleServerStatusCheck(HttpServletRequest req) throws Exception {
+  //Url check Call that statusCheck API port/RequestMapping/statusCheck ....
+  String url = "localhost:[port]/[RequestMapping]/statusCheck"; 
+  String status = "";
+  try {
+    HttpResponse<JsonNode> response = Unirest
+        .get(url)
+        .header("Content-Type", "application/json")
+        .asJson();
+    status = String.valueOf(response.getStatus());
+  } catch (Exception e) {
+    status = "500";
+  }
+
+  return status;
+}
